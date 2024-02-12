64aeca277d216dd487d54bfc6e8f1035a2acbac9
Yurem1 <yuremtovar1@gmail.com>
2024-02-12 17:41:35 -0600

    Refactor page component to include a form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Form from "@/components/form";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <main className="flex flex-col items-center justify-center h-screen bg-gray-50">
+      <Form/>
+    </main>
   );
 }
