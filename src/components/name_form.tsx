682b03ff5b4d5e2cce1954ce44b062348813c165
Yurem1 <yuremtovar1@gmail.com>
2024-02-12 22:04:49 -0600

    Refactor name_form component to use fieldsets and add input placeholders

--- src/components/name_form.tsx
+++ src/components/name_form.tsx
@@ -5,12 +5,13 @@ const InputName = React.forwardRef(function InputName(props, ref) {
 
     const labelForm = (): React.JSX.Element[] => {        
         return Object.entries(NAME_FORM).map(([key, value]) => {
-
             return (
-                <div key={key} className='flex flex-col'>
-                    <label>{value}</label>
-                    <input type='text'/>
-                </div>
+                <fieldset key={key}>
+                    <div className='flex flex-col'>
+                        <label>{value.input}</label>
+                        <input type='text' placeholder={value.placeholder}/>
+                    </div>
+                </fieldset>
             );
         });
     }
