3ac50c3dac28e4142d0e85efec87125d81c17588
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:04:49 -0600

    Add more properties to IForm, refactor IAction, refactor reducer, add findPayrate function

--- src/utility/data.tsx
+++ src/utility/data.tsx
@@ -1,30 +1,70 @@
-import React from 'react';
+import PAYRATE from '@/files/pay_rate.json';
+import { DEFAULT_FORM_DATA } from '@/utility/constants';
 
-type TForm = {
-    input: 'Name' | 'Last name' | 'ssn'
-}
-
+/**
+ * Represents the form data for an employee.
+ */
 export interface IForm {
     name: string;
     lastName: string;
     ssn: number;
+    hoursWorked: number;
+    payRate: number;
+    shift: string;
 }
 
+/**
+ * Represents an interface for actions.
+ * @interface IAction
+ */
 interface IAction {
-    name: TForm["input"];
-    value: string;
+    [key: string]: string
 }
 
+/**
+ * Utility class for handling data operations.
+ */
 export default class Data {
+
+    /**
+     * The default data for the form.
+     */
+    public static readonly DEFAULT_DATA = DEFAULT_FORM_DATA;
+
+    /**
+     * Reducer function for updating the form state based on the action.
+     * @param state - The current form state.
+     * @param action - The action to be performed on the form state.
+     * @returns The updated form state.
+     */
     public static reducer(state: IForm, action: IAction): IForm {
-        if(action.name) {
-            return {
-                ...state
+
+        for(const [stateKey] of Object.entries(state)) {
+            for(const [actionKey] of Object.entries(action)) {
+                if(stateKey === actionKey) {
+                    return {
+                        ...state, ...action
+                    }
+                }
             }
         }
 
-        return {
-            ...state
+        return state
+    }
+
+    /**
+     * Finds the payrate based on the given payrate key.
+     * @param payrate - The payrate key.
+     * @returns The corresponding payrate value.
+     */
+    public static findPayrate(payrate: string): number {
+
+        for(const [key, { rate }] of Object.entries(PAYRATE)) {
+            if(key === payrate) {
+                return rate;
+            }
         }
+        
+        return 0
     }
 }
