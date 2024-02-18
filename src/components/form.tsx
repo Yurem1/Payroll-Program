2035c806f46772fd571e32a7f796f79719b4132c
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:01:18 -0600

    Refactor form component to use formHandler, and add comments

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -7,25 +7,28 @@ import Submit from '@/components/submit';
 import Payrate from '@/components/payrate';
 import Hours from '@/components/hours';
 import Shift from '@/components/shift';
-import Data, { IForm } from '@/utility/data';
+import Data from '@/utility/data';
 
+/**
+ * Renders a form component.
+ *
+ * @returns The JSX element representing the form.
+ */
 export default function Form(): React.JSX.Element {
 
     const [state, dispatch] = React.useReducer(
-        Data.reducer, {
-            name: '',
-            lastName: '',
-            ssn: 0
-        } as IForm
+        Data.reducer, Data.DEFAULT_DATA
     );
 
 
-    const handler = (event: ChangeEvent<HTMLFormElement>) => {
-        console.log(event.target.name)
+    const formHandler = (event: ChangeEvent<HTMLFormElement>) => {
+        dispatch({
+            [event.target.name]: event.target.value
+        });
     }
 
     return (
-        <form className='bg-gray-100 rounded-xl border-black border p-5' onChange={handler}>
+        <form className='bg-gray-100 rounded-xl border-black border p-5' onChange={formHandler}>
             <div className='grid grid-cols-2 gap-5 bg-grey-50'>
                 <InputName/>
                 <SSNInput/>
