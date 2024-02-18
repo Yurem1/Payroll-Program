fd0b5ca23da48ce298527ee25a67c0b4e00453db
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:02:57 -0600

    Refactor SSNInput component to use lowercase 'ssn' for input name and add comments

--- src/components/ssn.tsx
+++ src/components/ssn.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { ssn_form as SSN_FORM } from '@/files/form.json';
 
+/**
+ * A component that renders an input field for Social Security Number (SSN).
+ * @param props - The component props.
+ * @param ref - The ref to be forwarded to the input element.
+ * @returns The rendered SSNInput component.
+ */
 const SSNInput = React.forwardRef(function SSNInput(props, ref) {
     return (
         <fieldset>
             <div className='flex flex-col'>
                 <label>Social Security Number (SSN)</label>
-                <input name='SSN' type='text' pattern='\d{3}-\d{2}-\d{4}' placeholder='123-45-6789'/>
+                <input name='ssn' type='text' pattern='\d{3}-\d{2}-\d{4}' placeholder='123-45-6789'/>
             </div>
         </fieldset>
     );
