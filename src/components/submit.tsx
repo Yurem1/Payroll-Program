1fd0b09e1d4266643db62f5c8459c553dbbfc0c3
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:03:01 -0600

    Add submit button component with props and ref

--- src/components/submit.tsx
+++ src/components/submit.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * A submit button component.
+ *
+ * @component
+ * @param {object} props - The props for the Submit component.
+ * @param {React.Ref} ref - The ref to attach to the Submit component.
+ * @returns {JSX.Element} The rendered Submit component.
+ */
 const Submit = React.forwardRef(function Submit(props, ref) {
     return (
         <fieldset className='col-span-2'>
