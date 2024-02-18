2fd247bf2ec3b220e04d410cdd9c74c3a9228718
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:02:44 -0600

    Update shift input name and add comments

--- src/components/shift.tsx
+++ src/components/shift.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+/**
+ * Represents a Shift component.
+ * @param {object} props - The props for the Shift component.
+ * @param {React.Ref} ref - The ref for the Shift component.
+ * @returns {JSX.Element} The rendered Shift component.
+ */
 const Shift = React.forwardRef(function Shift(props, ref) {
     return (
         <fieldset>
             <div className='flex flex-col'>
                 <label>Shift type</label>
-                <input name='Shift' type='text' placeholder='Evening shift'/>
+                <input name='shift' type='text' placeholder='Evening shift'/>
             </div>
         </fieldset>
     );
