a981c5a2c23f6ed1856ed6473e9266604cd3a62a
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:02:27 -0600

    Update payRate input name in Payrate component and add comments

--- src/components/payrate.tsx
+++ src/components/payrate.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+/**
+ * Represents the Payrate component.
+ * @param {object} props - The component props.
+ * @param {React.Ref} ref - The ref object.
+ * @returns {JSX.Element} The rendered Payrate component.
+ */
 const Payrate = React.forwardRef(function Payrate(props, ref) {
     return (
         <fieldset>
             <div className='flex flex-col'>
                 <label>Payrate</label>
-                <input name='Payrate' type='number' min={0} max={5} placeholder='0'/>
+                <input name='payRate' type='number' min={0} max={5} placeholder='0'/>
             </div>
         </fieldset>
     );
