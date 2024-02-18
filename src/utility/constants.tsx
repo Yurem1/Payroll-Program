cc5a33e21ff1eecda138fe01028b68eb6606f197
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:03:10 -0600

    Add constants file with default form data

---
+++ src/utility/constants.tsx
@@ -0,0 +1,14 @@
+import { IForm } from "@/utility/data";
+
+/**
+ * Default form data.
+ */
+export const DEFAULT_FORM_DATA: IForm = {
+    name: '',
+    lastName: '',
+    ssn: 0,
+    hoursWorked: 0,
+    payRate: 0,
+    shift: ''
+};
+
