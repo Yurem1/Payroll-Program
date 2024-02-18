6e0fdcbe8d06b652579de55f97ca4bede378858f
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:01:43 -0600

    Update input name in Hours component, and add comments

--- src/components/hours.tsx
+++ src/components/hours.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+/**
+ * Renders a component for entering hours worked.
+ * @param {object} props - The component props.
+ * @param {React.Ref} ref - The ref object.
+ * @returns {React.ReactNode} The rendered component.
+ */
 const Hours = React.forwardRef(function Hours(props, ref) {
     return (
         <fieldset>
             <div className='flex flex-col'>
                 <label>Hours Worked</label>
-                <input name='Hours' type='number' min={0} max={72} placeholder='0'/>
+                <input name='hoursWorked' type='number' min={0} max={72} placeholder='0'/>
             </div>
         </fieldset>
     );
