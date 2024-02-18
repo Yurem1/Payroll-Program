9871f524a91428e42c02a848294ef8e489a42a61
Yurem1 <yuremtovar1@gmail.com>
2024-02-17 19:02:17 -0600

    Add comments

--- src/components/name.tsx
+++ src/components/name.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { name_form as NAME_FORM } from '@/files/form.json'
 
+/**
+ * Renders an input form for the name component.
+ * @param props - The component props.
+ * @param ref - The ref to forward to the underlying input element.
+ * @returns The rendered input form.
+ */
 const InputName = React.forwardRef(function InputName(props, ref) {
 
+    /**
+     * Generates an array of JSX elements for the input form.
+     * @returns An array of JSX elements representing the input form.
+     */
     const labelForm = (): React.JSX.Element[] => {        
         return Object.entries(NAME_FORM).map(([key, value]) => {
             return (
