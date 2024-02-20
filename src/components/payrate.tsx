a1afd479566e884b0c7747f844027e375a4c1bf5
Yurem1 <yuremtovar1@gmail.com>
2024-02-19 22:14:22 -0600

    Add required attribute to payRate input field

--- src/components/payrate.tsx
+++ src/components/payrate.tsx
@@ -11,7 +11,8 @@ const Payrate = React.forwardRef(function Payrate(props, ref) {
         <fieldset>
             <div className='flex flex-col'>
                 <label>Payrate</label>
-                <input name='payRate' type='number' min={0} max={5} placeholder='0'/>
+                <input name='payRate' type='number' min={0} max={5} placeholder='0'
+                required/>
             </div>
         </fieldset>
     );
