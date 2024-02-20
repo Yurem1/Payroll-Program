286ea1acc02b5054599367feb6326121e765ba23
Yurem1 <yuremtovar1@gmail.com>
2024-02-19 22:14:32 -0600

    Add required attribute to hoursWorked input field

--- src/components/hours.tsx
+++ src/components/hours.tsx
@@ -11,7 +11,8 @@ const Hours = React.forwardRef(function Hours(props, ref) {
         <fieldset>
             <div className='flex flex-col'>
                 <label>Hours Worked</label>
-                <input name='hoursWorked' type='number' min={0} max={72} placeholder='0'/>
+                <input name='hoursWorked' type='number' min={0} max={72} placeholder='0' 
+                required/>
             </div>
         </fieldset>
     );
