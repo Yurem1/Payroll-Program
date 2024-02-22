9a65032a714ecb832643ceaf57c3b4aecc239590
Yurem1 <yuremtovar1@gmail.com>
2024-02-21 22:27:32 -0600

    Refactor form component

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -7,8 +7,6 @@ import Submit from '@/components/submit';
 import Payrate from '@/components/payrate';
 import Hours from '@/components/hours';
 import Shift from '@/components/shift';
-import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/navigation';
 
 /**
  * Renders a form component.
@@ -16,25 +14,11 @@ import { useRouter } from 'next/navigation';
  * @returns The JSX element representing the form.
  */
 export default function Form(): React.JSX.Element {
-
-    const router = useRouter();
-
-    const formHandler = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        const form = new FormData(event.currentTarget);
-        router.push('/result', )
-    }
-
     return (
-        <form className='bg-gray-100 rounded-xl border-black border p-5' onSubmit={formHandler}>
+        <form className='bg-gray-100 rounded-xl border-black border p-5' 
+        action='/result' method='get'>
             <div className='grid grid-cols-2 gap-5'>
-                <InputName/>
-                <SSNInput/>
-                <Shift/>
-                <Payrate/>
-                <Hours/>
-                <Submit/>
+                <InputName/> <SSNInput/> <Shift/> <Payrate/> <Hours/> <Submit/>
             </div>
         </form>
     );
