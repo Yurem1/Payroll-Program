d6e673268a6b1831210e731bc63f8598d54ddcb7
Yurem1 <yuremtovar1@gmail.com>
2024-02-21 22:27:50 -0600

    Refactor result page component and add search functionality

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,15 +1,62 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
+import Data from '@/utility/data';
 import React from 'react';
 
-export default function Page(props: any): React.JSX.Element {
-    console.log(props.lastName);
-    
+export default function Page(): React.JSX.Element {
+    const search = useSearchParams()
+
+    const [state, setState] = React.useState(Data.DEFAULT_DATA);
+
+    React.useEffect(() => {
+        search.forEach((value, key) => {
+            setState((prev) => ({
+                ...prev,
+                [key]: value
+            }));
+        });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    function displayResults(): React.JSX.Element[] {
+
+        return Object.entries(state).map(([key, value], index,) => {
+            return (
+                <div key={index} className='rounded'>
+                    <h1>{key.toLocaleUpperCase()}</h1>
+                    <p>{value}</p>
+                </div>
+            );
+        });
+    }
+
+    const returnPayrate = (): number => {
+        return Data.findPayrate(
+            state.payRate.toString()
+        );
+    }
+
+    const returnWeeklyRate = (): number => {
+        return Data.calculatePayrate(state);
+    }
+
     return (
-        <form className='bg-gray-100 rounded-xl border-black border p-5'>
-            <p>
-                hi
-            </p>
-        </form>
+        <div className='bg-gray-100 rounded-xl border-black border p-5'>
+            <div className='grid grid-cols-3 gap-5'>
+                <>
+                    {displayResults()}
+                </>
+                <div>
+                    <h1>SALARY</h1>
+                    <p>{`$${returnPayrate()}/hr`}</p>
+                </div>
+                <div>
+                    <h1>WEEKLY PAY</h1>
+                    <p>{returnWeeklyRate()}</p>
+                </div>
+            </div>
+        </div>
     );
 }
