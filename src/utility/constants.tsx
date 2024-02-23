ff7d31b0def91f7b1b06c8ca479a8934aef9dd91
Yurem1 <yuremtovar1@gmail.com>
2024-02-22 23:17:10 -0600

    Update default form data in constants.tsx

--- src/utility/constants.tsx
+++ src/utility/constants.tsx
@@ -4,11 +4,11 @@ import { IForm } from "@/utility/data";
  * Default form data.
  */
 export const DEFAULT_FORM_DATA: IForm = {
-    name: '',
-    lastName: '',
+    name: 'NAME',
+    lastName: 'LAST_NAME',
     ssn: 0,
     hoursWorked: 0,
     payRate: 0,
-    shift: ''
+    shift: 'SHIFT'
 };
 
