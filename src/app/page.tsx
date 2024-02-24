494ad804611d691dd192ef4b5d26e37ec09032e7
Yurem1 <yuremtovar1@gmail.com>
2024-02-23 18:06:16 -0600

    transferred form.tsx contents into page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 'use client';
 
-import Form from "@/app/form";
+import React from 'react';
+import InputName from '@/components/name';
+import SSNInput from '@/components/ssn';
+import Submit from '@/components/submit';
+import Payrate from '@/components/payrate';
+import Hours from '@/components/hours';
+import Shift from '@/components/shift';
 
-export default function Home() {
+/**
+ * Renders the main page (a form component) 
+ * @returns The JSX element representing the form.
+ */
+export default function Page(): React.JSX.Element {
   return (
     <main>
-      <Form/>
+      <form className='bg-gray-100 rounded-xl border-black border p-5' action='/result' method='get'>
+        <div className='grid grid-cols-2 gap-5'>
+          <InputName />
+          <SSNInput />
+          <Shift />
+          <Payrate />
+          <Hours />
+          <Submit />
+        </div>
+      </form>
     </main>
   );
 }
